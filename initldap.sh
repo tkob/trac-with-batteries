f1ed4f3e0e865025fab34afeafcc3da4d6e0396d
tkob <ether4@gmail.com>
2016-08-07 11:00:41 +0900

    Move password setup command to initldap.sh

--- initldap.sh
+++ initldap.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+ldapadd -Y EXTERNAL -H ldapi:// -f /usr/local/share/applications/olcRootPW.ldif
 ldapadd -x -D "cn=Manager,dc=my-domain,dc=com" -w secret -f /usr/local/share/applications/init.ldif
