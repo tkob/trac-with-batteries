31974a9c8674c5e5625b54a9ca273dbaa7174fe3
tkob <ether4@gmail.com>
2016-11-19 16:48:32 +0900

    Fix directory name of TestLink

--- install-testlink.sh
+++ install-testlink.sh
@@ -2,4 +2,4 @@
 
 wget -O /tmp/testlink-1.9.15.tar.gz 'https://github.com/TestLinkOpenSourceTRMS/testlink-code/archive/1.9.15.tar.gz'
 (cd /tmp; tar xzf testlink-1.9.15.tar.gz)
-mv /tmp/testlink-1.9.15 /var/www/html/testlink
+mv /tmp/testlink-code-1.9.15 /var/www/html/testlink
